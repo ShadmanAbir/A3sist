ba2ada6b8d5675c86c1020353f38ce3c1d8e4c18
Shadman Sakib <shadmanabir@outlook.com>
2025-08-08 13:06:11 +0600

    Add new UI tool window and related components for Visual Studio extension

---
+++ UI/A3ToolWindowCommand.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    /// <summary>
+    /// A command for showing a tool window.
+    /// </summary>
+    [VisualStudioContribution]
+    public class A3ToolWindowCommand : Command
+    {
+        /// <inheritdoc />
+        public override CommandConfiguration CommandConfiguration => new(displayName: "732e41d0-846d-418a-acef-f135b9f75e41")
+        {
+            // Use this object initializer to set optional parameters for the command. The required parameter,
+            // displayName, is set above. To localize the displayName, add an entry in .vsextension\string-resources.json
+            // and reference it here by passing "%UI.A3ToolWindowCommand.DisplayName%" as a constructor parameter.
+            Placements = [CommandPlacement.KnownPlacements.ExtensionsMenu],
+            Icon = new(ImageMoniker.KnownValues.Extension, IconSettings.IconAndText),
+        };
+
+        /// <inheritdoc />
+        public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+        {
+            await this.Extensibility.Shell().ShowToolWindowAsync<A3ToolWindow>(activate: true, cancellationToken);
+        }
+    }
+}
